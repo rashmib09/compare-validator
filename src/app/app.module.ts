f885b7c7eaef093579ed56e83ffe62d0d722b887
rashmi <rashmigcoea@gmail.com>
2020-08-13 00:58:38 +0530

    material commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { MaterialModule } from 'src/app/shared/material.module';
+import { MaterialModule } from './shared/material.module';
 
 @NgModule({
   declarations: [
